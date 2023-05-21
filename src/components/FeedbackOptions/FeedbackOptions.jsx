fa4344edd7f47ad9434f1e04ffdc72520b3daa3d
Stanislav-Zhuk <steys.ua@gmail.com>
2023-05-21 16:49:09 +0300

    little fix

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -21,8 +21,8 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 }
 
 FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
-  options: PropTypes.object.isRequired,
 };
 
 export default FeedbackOptions;
